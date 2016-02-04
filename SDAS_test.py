9534fe4c911e1e48d1cf137acc7389663875a550
J. P.S. Loureiro <jpsloureiro@ipfn.ist.utl.pt>
2016-02-04 14:56:31 +0000

    Cleaned the code in SDAS_test script

--- SDAS_test.py
+++ SDAS_test.py
@@ -6,22 +6,23 @@ import sys
 from pyISTTOK.special_mean_val import special_mean_val
 from pyISTTOK.exposure_time import exposure_time
 from pyISTTOK.period_counter import period_counter
-#import pyISTTOK as i
 import xmlrpclib
 
 #####################################################################
-densthresh=2.e18
-iplthresh=1.e3
-martethresh=10
-plok=1
-deok=1
-ivok=1
+##                  GLOBAL VARIABLES                               ##
+#####################################################################
+iplasma_threshold=1.e3
+dens_threshold=2.e18
+marte_threshold=10
+iplasma_ok=1
+dens_ok=1
+marte_ok=1
 #####################################################################
 
 client = StartSdas()
 
 if len(sys.argv) < 2 :
-    shotnr = client.searchMaxEventNumber('0x0000')	
+    shotnr = client.searchMaxEventNumber('0x0000')
 else :
     shotnr = int(sys.argv[-1])
 
@@ -34,57 +35,57 @@ print 'Loading data'
 
 try:
     [iplasma,iplasma_times] = LoadSdasData(client, plasma_curr_channelID, shotnr);
-    plok=numpy.all(numpy.isfinite(iplasma))
+    iplasma_ok=numpy.all(numpy.isfinite(iplasma))
 except xmlrpclib.Fault:
-    print 'DATA NOT READY'
-    plok=0
+    print 'PLASMA CURRENT DATA NOT READY'
+    iplasma_ok=0
 try:
     [dens,dens_times] =       LoadSdasData(client, plasma_dens_channelID, shotnr);
-    deok=numpy.all(numpy.isfinite(dens))  # check if all finite
+    dens_ok=numpy.all(numpy.isfinite(dens))  # check if all finite
 except xmlrpclib.Fault:
-    print 'DATA NOT READY'
-    deok=0
+    print 'DENSITY DATA NOT READY'
+    dens_ok=0
 try:
     [marte,marte_times] =       LoadSdasData(client, marte_power_channelID, shotnr);
-    ivok=numpy.all(numpy.isfinite(marte))  # check if all finite
+    marte_ok=numpy.all(numpy.isfinite(marte))  # check if all finite
 except xmlrpclib.Fault:
-    print 'DATA NOT READY'
-    ivok=0
+    print 'MARTE CONTROL DATA NOT READY'
+    marte_ok=0
 
 
 print 'Data loaded\n'
 
 
-if plok:
-    print 'FROM IPLASMA   ( thresh',iplthresh,')'
-    exposure_time(numpy.abs(iplasma),iplasma_times,iplthresh)
-    print 'Mean current {0:.3f} kA'.format(special_mean_val(numpy.abs(iplasma),iplthresh)/1e3)
-    iplasma_periods = period_counter(numpy.abs(iplasma),iplthresh)
+if iplasma_ok:
+    print 'FROM IPLASMA   ( thresh',iplasma_threshold,')'
+    exposure_time(numpy.abs(iplasma),iplasma_times,iplasma_threshold)
+    print 'Mean current {0:.3f} kA'.format(special_mean_val(numpy.abs(iplasma),iplasma_threshold)/1e3)
+    iplasma_periods = period_counter(numpy.abs(iplasma),iplasma_threshold)
     print 'I counted '+str(iplasma_periods)+' periods'
 else:
-    print 'NO IPLASMA DATA'	
+    print 'NO IPLASMA DATA'
 
 print ''
 
-if deok:
-    print 'FROM DENSITY   ( thresh',densthresh,')'
-    exposure_time(dens,dens_times,densthresh)
-    print 'Mean density {0:.2e} m'.format(special_mean_val(dens,densthresh))+u'\u207b\u00b3'
-    dens_periods = period_counter(dens,densthresh)
+if dens_ok:
+    print 'FROM DENSITY   ( thresh',dens_threshold,')'
+    exposure_time(dens,dens_times,dens_threshold)
+    print 'Mean density {0:.2e} m'.format(special_mean_val(dens,dens_threshold))+u'\u207b\u00b3'
+    dens_periods = period_counter(dens,dens_threshold)
     print 'I counted '+str(dens_periods)+' periods'
 else:
     print 'NO DENSITY DATA'
 
 print ''
 
-if ivok:
+if marte_ok:
     print 'FROM MARTE'
-    marte_periods = period_counter(abs(marte),martethresh,numpy.append(numpy.ones(10),numpy.zeros(1)))
+    marte_periods = period_counter(abs(marte),marte_threshold,numpy.append(numpy.ones(10),numpy.zeros(1)))
     print 'I counted '+str(marte_periods)+' periods'
 else:
     print 'NO MARTE DATA'
 
-if ivok and plok and deok:
+if marte_ok and iplasma_ok and dens_ok:
     print '\nSucess rate was '+str(iplasma_periods)+'/'+str(marte_periods)
     if iplasma_periods==marte_periods:
         print '!!!CONGRATULATIONS!!!'
@@ -92,4 +93,3 @@ if ivok and plok and deok:
         print '!!!BOOOOOO!!!'
 
 print '\n'
-
