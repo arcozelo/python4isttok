d9b7ccc30719954967caf86ac26fe0312c38b517
J. P.S. Loureiro <jpsloureiro@ipfn.ist.utl.pt>
2016-02-09 15:52:05 +0000

    Changed the default test function in period counter to get a more acurate number

--- pyISTTOK/period_counter.py
+++ pyISTTOK/period_counter.py
@@ -1,13 +1,13 @@
 import numpy
 
-def period_counter(input_data, threshold, tester = numpy.append(numpy.ones(200),numpy.zeros(1))):
+def period_counter(input_data, threshold, tester = numpy.append(numpy.ones(100),numpy.zeros(1))):
     count = 0
     #tester = numpy.append(numpy.ones(200),numpy.zeros(1));
-    
-    mask = input_data > threshold; 
-    
+
+    mask = input_data > threshold;
+
     for i in range(len(mask[250:])-len(tester)):
         if numpy.sum(numpy.equal(mask[250+i:250+i+len(tester)],tester))==len(tester):
 	    count+=1
-    
+
     return count;
